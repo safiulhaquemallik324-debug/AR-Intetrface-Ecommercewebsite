fd68bca1bceb2dbf36bd46e07e57175355c4d850
safiul haque mallik <safiulhaquemallik324@gmail.com>
2026-06-28 15:52:50 +0530

    Setup GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ar-interface-website/",
-})
+  base: "/AR-Intetrface-Ecommercewebsite/",
+});
